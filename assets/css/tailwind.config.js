e3504ea1542681379da5b5047babbca1669bda5d
Sam Killermann <sK@hues.xyz>
2020-03-24 11:38:24 -0500

    basic coming soon one-pager

---
+++ assets/css/tailwind.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  theme: {
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+    },
+    extend: {
+      fontFamily: {
+        sans: ['sofia-pro', 'sans-serif']
+      },
+      colors: {
+        gray: {
+          '100': '#fff1ce',
+          '200': '#e8deca',
+          '300': '#dbd7ce',
+          '400': '#bab7b1',
+          '500': '#999792',
+          '600': '#706e69',
+          '700': '#35322d',
+          '800': '#38352e',
+          '900': '#191714',
+        }
+      }
+    }
+  }
+}
